5224d3f56e11b45cbcfe5605d9b7f13fe26720ad
j.kalous <j.kalous@quadient.com>
2018-11-01 08:35:28 +0100

    update commit

--- src/test/java/AddTest.java
+++ src/test/java/AddTest.java
@@ -7,4 +7,9 @@ public class AddTest{
         assert 1 + 1  == 2;
     }
 
+    @Test
+    public void SubtractionTest() { assert 2 - 1 == 33; }
+
+    @Test
+    public void SubtractionTestTwo() { assert 2 - 1 == 1; }
 }
